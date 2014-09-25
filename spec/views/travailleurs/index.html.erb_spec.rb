34851f7685fd6f6e3fdb962d58c56a81302ec832
Prelang Builder <builder@prelang.com>
2014-09-25 09:40:48 +0000

    Generated scaffolding for model 'Travailleur' via Rails

---
+++ spec/views/travailleurs/index.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "travailleurs/index", :type => :view do
+  before(:each) do
+    assign(:travailleurs, [
+      Travailleur.create!(
+        :nom => "Nom",
+        :principal_groupe => nil,
+        :sous_groupe => nil,
+        :agence => nil
+      ),
+      Travailleur.create!(
+        :nom => "Nom",
+        :principal_groupe => nil,
+        :sous_groupe => nil,
+        :agence => nil
+      )
+    ])
+  end
+
+  it "renders a list of travailleurs" do
+    render
+    assert_select "tr>td", :text => "Nom".to_s, :count => 2
+    assert_select "tr>td", :text => nil.to_s, :count => 2
+    assert_select "tr>td", :text => nil.to_s, :count => 2
+    assert_select "tr>td", :text => nil.to_s, :count => 2
+  end
+end
