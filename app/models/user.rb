d8f5b49220464475a756f879c82fe04792b7cbbe
Prelang Builder <builder@prelang.com>
2014-09-25 09:40:42 +0000

    Installed relation 'belongs_to' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
   belongs_to :principal_groupe
   belongs_to :sous_groupe
   belongs_to :agence
+  belongs_to :travailleur
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
